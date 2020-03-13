317366fd2bf439253173e9b1c71dc93f34f82d14
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-03-13 22:39:16 +1030

    displayQuestions function fixed.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var timeElement = document.querySelector('.time');
-var questionsElement = document.querySelector('questions-rendered');
+var timeEl = document.querySelector(".time");
+var questionsEl = document.querySelector(".questions-rendered");
 
-var secRemaining = 75;
-var secPenalty = 10;
+var secondsRemaining = 75;
+var penaltySeconds = 10;
 var timerInterval;
 var numCorrect = 0;
 var questionIndex = 0;
@@ -10,138 +10,175 @@ var highScores;
 var highScoresArray = [];
 var score = 0;
 
+var questions = [
+    { 
+        title: "How can you get the type of arguments passed to a function?",
+        choices: ["A. Using type of operator", "B. Using getType function", "C. Both of the above", "D. None of the above"],
+        correctAnswer: "A. Using type of operator"
+       
+    },
+    {
+        title: "What language is used to style webpages?",
+        choices: ["MySql", "Node", "CSS", "Spanish"],
+        correctAnswer: "CSS"        
+    },
+    {
+        title: "Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+        choices: ["A. toSource()", "B. valueOf()", "C. toString()", "D. None of the above"],
+        correctAnswer: "B. valueOf()"
+    },
+    {
+      
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        title: "Which of the following function of String object returns the index within the calling String object of the first occurence of the specified value?",
+        choices: ["A. substr()", "B. search()", "C. lastIndexOf()", "D. indexOf()"],
+        correctAnswer: "D. indexOf()"
+    }
+  ];
+  
 
-const startButton = document.getElementById("start");
-const scoreButton = document.getElementById("viewScores");
-const initialsEnter = document.getElementById("initials");
-const showScores = document.getElementById("scores");
+const startBtn = document.getElementById("start");
+const viewScoresBtn = document.getElementById("viewScores");
+const initialsDiv = document.getElementById("initials");
+const scoresDiv = document.getElementById("scores");
 
-
+// Starts timer
 function setTime() {
-    timerInterval = setInterval(function() {
-        secRemaining--;
-        timeElement.textContent = "Time Remaining: " + secRemaining;
-        if (secRemaining === 0) {
-            finish();
-        }
-    }, 1000);
+  timerInterval = setInterval(function() {
+    secondsRemaining--;
+    timeEl.textContent = "Time Remaining: " + secondsRemaining;
+    if (secondsRemaining === 0) {
+      finish();
+    }
+  }, 1000);
 }
 
 function finish() {
-
-    clearInterval(timerInterval);
-
-    secRemaining = 0;
-
-    document.querySelector(".time").innerHTML = "Done";
-
-    questionsElement.textContent = "";
-
-    score = numCorrect * (100 / questions.length);
-
-    document.getElementById("choice-response").innerHTML = 
-        "Your final score is: " + score;
-
-    initialsEnter.style.display = "block";
-
-    document.getElementById("myInitials").value = "";
+  // Stops timer
+  clearInterval(timerInterval);
+  // Sets variable to zero
+  secondsRemaining = 0;
+  // Changes time remaining heading to "Done" when the quiz is complete
+  document.querySelector(".time").innerHTML = "Done";
+  // Clears out last question displayed
+  questionsEl.textContent = "";
+  // Calculates score
+  score = numCorrect * (100 / questions.length);
+  // Displays final score
+  document.getElementById("choice-response").innerHTML =
+    "Your final score is: " + score;
+  // Pops up div to enter initials
+  initialsDiv.style.display = "block";
+  // Clears out textbox of previous user initials; empty box every time
+  document.getElementById("myInitials").value = "";
 }
 
-
-
+// Gets initials after selecting submit button
+// Does not validate whether or not initials were entered
 function getInitials() {
-    if (highScoresArray.length === 0) {
-        highScores = document.getElementById("myInitials").value + " - " + score;
-    } else {
-        highScores = 
-        " " + document.getElementById("myInitials").value + " " + score;
-    }
+  if (highScoresArray.length === 0) {
+    highScores = document.getElementById("myInitials").value + " - " + score;
+  } else {
+    highScores =
+      " " + document.getElementById("myInitials").value + " - " + score;
+  }
 
-    highScoresArray.push(highScores);
-    initialsEnter.style.display = "none";
-    document.querySelector("time").innerHTML = "High Scores";
-    document.getElementById("choice-response").innerHTML = highScoresArray;
-    showScores.style.display = "block";
+  highScoresArray.push(highScores);
+  initialsDiv.style.display = "none";
+  document.querySelector(".time").innerHTML = "High Scores";
+  document.getElementById("choice-response").innerHTML = highScoresArray;
+  scoresDiv.style.display = "block";
 }
 
-
-
+// If "Go Back" button is clicked, this function is triggered to go back to the start page
 function startOver() {
-    document.getElementById("choice-response").innerHTML = "";
-    showScores.style.display = "none";
-    timeElement.textContent = "Time Remaining: 0";
-    startButton.style.display = "initial";
-    scoreButton.style.display = "initial";
+  document.getElementById("choice-response").innerHTML = "";
+  scoresDiv.style.display = "none";
+  timeEl.textContent = "Time Remaining: 0";
+  startBtn.style.display = "initial";
+  viewScoresBtn.style.display = "initial";
+  document.getElementById("instructions").innerHTML =
+    "As a coding bootcamp student, I want to take a timed quiz on JavaScript fundamentals that stores high scores so I can gauge my progrees compared to my peers.";
 }
 
-
+// If clicked, will show high scores in the array, but if array is empty, shows "no scores"
 function viewAllScores() {
-    if (highScoresArray.length  === 0) {
+  if (highScoresArray.length === 0) {
     document.getElementById("choice-response").innerHTML = "No Scores: ";
-    } else {
-        document.getElementById("choice-response").innerHTML = 
-        "High Scores: " + highScoresArray;
-    }
+  } else {
+    document.getElementById("choice-response").innerHTML =
+      "High Scores: " + highScoresArray;
+  }
 }
 
+// Empties array when "Clear High Scores" is clicked
 function clearScores() {
-    highScoresArray = [];
-    document.getElementById("choice-response").innerHTML = highScoresArray;
+  highScoresArray = [];
+  document.getElementById("choice-response").innerHTML = highScoresArray;
 }
 
-startButton.addEventListener("click", function() {
-    secRemaining = 75;
-    setTime();
+// Resetting values needed to start a new quiz
+startBtn.addEventListener("click", function() {
+  secondsRemaining = 75;
+  setTime();
 
-    numCorrect = 0;
-    questionIndex = 0;
+  numCorrect = 0;
+  questionIndex = 0;
 
-    startButton.style.display = "none";
-    scoreButton.style.display = "none";
+  startBtn.style.display = "none";
+  viewScoresBtn.style.display = "none";
 
-    document.getElementById("choice-response").innerHTML = "";
-    document.getElementById("instructions").innerHTML = "";
-    displayQuestions();
+  document.getElementById("choice-response").innerHTML = "";
+  document.getElementById("instructions").innerHTML = "";
+
+  displayQuestions();
 });
 
+// Displays question and answers from the array one after the other
 function displayQuestions() {
-    if (secRemaining <= 0 || questionIndex >= questions.length) {
-        finish();
-        return;
-    }
-    questionsElement.textContent = "";
+  if (secondsRemaining <= 0 || questionIndex >= questions.length) {
+    finish();
+    return;
+  }
+  questionsEl.textContent = "";
 
-    var question = questions[questionIndex];
-    var questionDiv = document.createElement("div");
-    var questionText = document.createElement("p");
+  var question = questions[questionIndex];
+  var questionDiv = document.createElement("div");
+  var questionText = document.createElement("p");
 
-    questionText.textContent = question.title;
+  questionText.textContent = question.title;
 
-    questionDiv.appendChild(questionText);
+  questionDiv.appendChild(questionText);
 
-    for (i = 0; i < question.choices.length; i++) {
-        var option = document.createElement("button");
+  for (i = 0; i < question.choices.length; i++) {
+    var option = document.createElement("button");
 
-        option.textContent = question.choices[i];
+    option.textContent = question.choices[i];
 
-        option.style.cssText = 
-            "display: block; padding: 5px; margin-left: 40%; margin-bottom: 5px; width: 175px";
-        option.setAttribute("class", "option");
+    option.style.cssText =
+      "display: block; padding: 5px; margin-left: 40%; margin-bottom: 5px; width: 175px;";
+    option.setAttribute("class", "option");
 
-        option.addEventListener("click", function(e) {
-            var optionClicked = e.target.innerHTML;
-            if (optionClicked === questions[questionIndex].correctAnswer){
-                numCorrect++;
-                document.getElementById("choice-response").innerHTML = "correct";
-                displayQuestions(questionIndex++);
-            } else {
+    option.addEventListener("click", function(e) {
+      var optionClicked = e.target.innerHTML;
+      if (optionClicked === questions[questionIndex].correctAnswer) {
+        // Variable is keeping track of the correct answers
+        numCorrect++;
+        document.getElementById("choice-response").innerHTML = "correct";
+        displayQuestions(questionIndex++);
+      } else {
+        // Incorrect answers result in a time penalty
+        secondsRemaining = secondsRemaining - penaltySeconds;
+        document.getElementById("choice-response").innerHTML = "wrong";
+        displayQuestions(questionIndex++);
+      }
+    });
+    questionDiv.appendChild(option);
+  }
 
-                secRemaining = secRemaining - secPenalty;
-                document.getElementById("choice-response").innerHTML = "wrong";
-                displayQuestions(questionIndex++);
-            }
-        });
-        questionDiv.appendChild(option);
-    }
-    questionsElement.appendChild(questionDiv);
+  questionsEl.appendChild(questionDiv);
 }
